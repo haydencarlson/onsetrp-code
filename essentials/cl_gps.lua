f13cef4e7e57931289f7e1213d942ca2672f0a39
Your Name <you@example.com>
2019-12-20 17:14:16 -0800

    Update waypoints

--- essentials/cl_gps.lua
+++ essentials/cl_gps.lua
@@ -3,7 +3,35 @@ local _ = function(k,...) return ImportPackage("i18n").t(GetPackageName(),k,...)
 
 local gpsmenu
 local gpsmain
-
+local teleportPlace = {
+    gas_station = { 125773, 80246, 1645 },
+    town = { -182821, -41675, 1160 },
+    prison = { -167958, 78089, 1569 },
+    diner = { 212405, 94489, 1340 },
+    shopp1 = { 128000, 77622, 1576 },
+    shopp2 = { 42600, 137926, 1581 },
+    shopp3 = { -15300, -2773, 2065 },
+    cockinbell = { 194206, 177201, 1307 },
+    bar = { 49100, 133316, 1578 },
+    shopp4 = { -169000, -39441, 1149 },
+    weed_gather_zone = { 186601, -39031, 1451 },
+    weed_process_zone = { 70695, 9566, 1366 },
+    heroin_gather_zone = { 186474, -43277, 1451 },
+    heroin_process_zone = { 73218, 3822, 1367 },
+    meth_gather_zone = { 193607, -46512, 1451 },
+    meth_process_zone = { 72095, 1418, 1367 },
+    wealthbank = { 211925, 191382, 1306 },
+    coke_gather_zone = { 192080, -45155, 1529 },
+    coke_process_zone = { 71981, 106, 1367 },
+    mining_gather_zone = { 32853, 98521, 1849 },
+    mining_process_zone = { 2427, 98041, 1497 },
+    mining_sell_zone = { 21799, 137848, 1555 },
+    drugs_sell_zone = { -177344, 3673, 1992 },
+    home_cardealer = { 161069, 191846, 1322 },
+    train_station = { 134704, 209961, 1292 },
+    old_cardealer = { 127720, 80774, 1567 },
+    delivery_npc_location = { -16925, -29058, 2200 }
+}
 
 AddEvent("OnKeyPress", function( key )
     if key == "G" then
@@ -25,110 +53,44 @@ end)
 
 
 AddEvent("OnTranslationReady", function()
-    gpsmain = Dialog.create(_("gps_menu"), nil,"Create waypoint", "Delete waypoint", "Locations", _("cancel"))
-    gpsmenu = Dialog.create(_("gps_menu"), nil, _("gps_home"), _("gps_spawn"), _("gps_vg"), _("gps_fuel"), _("gps_vd"), _("cockinbell"), _("gps_bar"), _("gunshop"), _("delivery_job"), _("cancel"))
+	gpsmain = Dialog.create(_("gps_menu"), nil,"Create waypoint", "Delete waypoint", "Locations", _("cancel"))
+	gpsmenu = Dialog.create(_("gps_menu"), nil, _("set_waypoint"), _("cancel"))
+	Dialog.addSelect(gpsmenu, 1, _("location"), 8)
+	local tpPlace = {}
+    for k,v in pairs(teleportPlace) do
+        tpPlace[k] = _(k)
+	end
+	Dialog.setSelectLabeledOptions(gpsmenu, 1, 1, tpPlace)
 end)
 
 
-AddEvent("OnDialogSubmit", function(dialog, button)
-       if dialog == gpsmain then
+AddEvent("OnDialogSubmit", function(dialog, button, ...)
+	if dialog == gpsmain then
 		if button == 1 then
-		local x, y, z = GetPlayerLocation()
-		if currentWaypoint ~= nil then
-        	DestroyWaypoint(currentWaypoint)
-    		end
+			local x, y, z = GetPlayerLocation()
+			if currentWaypoint ~= nil then
+			DestroyWaypoint(currentWaypoint)
+		end
 		currentWaypoint = CreateWaypoint(x, y, z, "Important Location")
-    		end
-		if button == 2 then
+	end
+	if button == 2 then
 		if currentWaypoint ~= nil then
-        	DestroyWaypoint(currentWaypoint)
-    		end
-    		end
-		if button == 3 then
+			DestroyWaypoint(currentWaypoint)
+		end
+	end
+	if button == 3 then
 		Dialog.show(gpsmenu)
-    		end
-		if button == 4 then
-    		end
+	end
+	if button == 4 then
+		end
  	end
 	if dialog == gpsmenu then
+		local args = { ... }
 		if button == 1 then
-		local x, y, z = 178804, 196523, 1315 
-		if currentWaypoint ~= nil then
-        	DestroyWaypoint(currentWaypoint)
-    		end
-		currentWaypoint = CreateWaypoint(x, y, z, "Home City")
+			local chords = teleportPlace[args[1]]
+			CreateWaypoint(chords[1], chords[2], chords[3], _(args[1]))
 		end
-		
-		if button == 2 then
-		local x, y, z = 170429, 204185, 1411
-		if currentWaypoint ~= nil then
-        	DestroyWaypoint(currentWaypoint)
-    		end
-		currentWaypoint = CreateWaypoint(x, y, z, "Store")
-		end
-		
-		if button == 3 then
-		local x, y, z = 206936, 179139, 1312
-		if currentWaypoint ~= nil then
-        	DestroyWaypoint(currentWaypoint)
-    		end
-		currentWaypoint = CreateWaypoint(x, y, z, "Garage")
-		end
-		
-		if button == 4 then
-		local x, y, z = 170389, 206425, 1410
-		if currentWaypoint ~= nil then
-        	DestroyWaypoint(currentWaypoint)
-    		end
-		currentWaypoint = CreateWaypoint(x, y, z, "Gas Station")
-		end
-
-		if button == 5 then
-		local x, y, z = 207009, 171182, 1330
-		if currentWaypoint ~= nil then
-        	DestroyWaypoint(currentWaypoint)
-    		end
-		currentWaypoint = CreateWaypoint(x, y, z, "Car Dealer")
-		end
-		
-		if button == 6 then
-		local x, y, z = 192906, 176674, 1324
-		if currentWaypoint ~= nil then
-        	DestroyWaypoint(currentWaypoint)
-    		end
-		currentWaypoint = CreateWaypoint(x, y, z, "Cock In Bell")
-		end
-		
-		if button == 7 then
-		local x, y, z = 49100, 133316, 1578
-		if currentWaypoint ~= nil then
-        	DestroyWaypoint(currentWaypoint)
-    		end
-		currentWaypoint = CreateWaypoint(x, y, z, "Bar")
-		end
-		
-		if button == 8 then
-		local x, y, z = 206321, 192627, 1358
-		if currentWaypoint ~= nil then
-        	DestroyWaypoint(currentWaypoint)
-    		end
-		currentWaypoint = CreateWaypoint(x, y, z, "Gun Shop")
-		end
-		
-		if button == 9 then
-		local x, y, z = 202381, 170223, 1306
-		if currentWaypoint ~= nil then
-        	DestroyWaypoint(currentWaypoint)
-    		end
-		currentWaypoint = CreateWaypoint(x, y, z, "Delivery Job")
-		end
-
-		
-		if button == 10 then
-		end
-
-		
-   	 end
+	end
 end)
 
 AddRemoteEvent("ClientDestroyCurrentWaypoint", function()
