268b9574a27cc683dba0ceeaa3088e6e5d535ed4
Your Name <you@example.com>
2020-01-04 17:10:29 -0800

    Fix rape

--- essentials/interactions/server.lua
+++ essentials/interactions/server.lua
@@ -1,46 +1,42 @@
-AddEvent("rape", function(player)
-    victim = GetNearestPlayer(player)
-    local rapefail = "You have failed to rape ".. PlayerData[victim].name
-    local rapefailvic = "You feel your anus expand..."
-    local rapesuc = "You have raped ".. PlayerData[victim].name
-    local rapesucvic = "You have been raped by " .. PlayerData[player].name
-
-    debug = victim
-    outcome = Random(1, 3)
-    rapehp = Random(25, 100)
-    if GetNearestPlayer(player) == nil then
-        AddPlayerChat(player, "No one is in range.")
-    elseif outcome > 2 then
-        SetPlayerHealth(victim, -rapehp) 
-        SetPlayerHealth(player, 100)
-        CallRemoteEvent(victim, "AidsOn")
-        AddPlayerChat(victim, rapesucvic)
-        AddPlayerChat(player, rapesuc)
-
-    else
-        AddPlayerChat(player, rapefail)
-        AddPlayerChat(player, debug)
-        AddPlayerChat(victim, rapefailvic)
-    end
-
-end)
-
-
-
-function GetNearestPlayer(player)
+local function GetNearestPlayer(player)
 	local x, y, z = GetPlayerLocation(player)
-	
-	for k,v in pairs(GetStreamedPlayersForPlayer(player)) do
-        local x2, y2, z2 = GetPlayerLocation(v)
-		local dist = GetDistance3D(x, y, z, x2, y2, z2)
-
-		if dist < 50 then
-			return k
-		end
+    for k, v in pairs(GetStreamedPlayersForPlayer(player)) do
+        if k ~= player then
+            local x2, y2, z2 = GetPlayerLocation(k)
+            local dist = GetDistance3D(x, y, z, x2, y2, z2)
+            if dist < 150 then
+                return k
+            end
+        end
 	end
-
 	return 0
 end
+
+AddEvent("rape", function(player)
+    local instigator = player
+    local victim = GetNearestPlayer(instigator)
+    if victim ~= 0 then
+        local rapefail = "You have failed to rape ".. PlayerData[victim].name
+        local rapefailvic = "You feel your anus expand..."
+        local rapesuc = "You have raped ".. PlayerData[victim].name
+        local rapesucvic = "You have been raped by " .. PlayerData[instigator].name
+
+        local outcome = Random(1, 3)
+        local rapehp = Random(25, 100)
+        if outcome > 2 then
+            local current_health = GetPlayerHealth(player)
+            SetPlayerHealth(victim, current_health - rapehp) 
+            SetPlayerHealth(instigator, 100)
+            CallRemoteEvent(victim, "AidsOn")
+            AddPlayerChat(victim, rapesucvic)
+            AddPlayerChat(instigator, rapesuc)
+
+        else
+            AddPlayerChat(instigator, rapefail)
+            AddPlayerChat(victim, rapefailvic)
+        end
+    end
+end)
 
 AddCommand("rape", function(player, instigator)
       CallEvent("rape", player, instigator)
