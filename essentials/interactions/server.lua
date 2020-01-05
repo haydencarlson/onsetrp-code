a6e97955b715394df747acef5bc22057f8f00c22
Your Name <you@example.com>
2020-01-04 16:30:11 -0800

    Fix rape

--- essentials/interactions/server.lua
+++ essentials/interactions/server.lua
@@ -1,9 +1,9 @@
 AddEvent("rape", function(player)
     victim = GetNearestPlayer(player)
-    local rapefail = "You have failed to rape "..victim
+    local rapefail = "You have failed to rape ".. PlayerData[victim].name
     local rapefailvic = "You feel your anus expand..."
-    local rapesuc = "You have raped "..victim
-    local rapesucvic = "You have been raped by " ..player
+    local rapesuc = "You have raped ".. PlayerData[victim].name
+    local rapesucvic = "You have been raped by " .. PlayerData[player].name
 
     debug = victim
     outcome = Random(1, 3)
@@ -13,19 +13,14 @@ AddEvent("rape", function(player)
     elseif outcome > 2 then
         SetPlayerHealth(victim, -rapehp) 
         SetPlayerHealth(player, 100)
-
         CallRemoteEvent(victim, "AidsOn")
-
         AddPlayerChat(victim, rapesucvic)
         AddPlayerChat(player, rapesuc)
-        AddPlayerChat(player, debug)
 
     else
-
         AddPlayerChat(player, rapefail)
         AddPlayerChat(player, debug)
         AddPlayerChat(victim, rapefailvic)
-        
     end
 
 end)
@@ -35,12 +30,11 @@ end)
 function GetNearestPlayer(player)
 	local x, y, z = GetPlayerLocation(player)
 	
-	for k,v in pairs(GetAllPlayers()) do
-        local x2, y2, z2 = GetAllPlayers(v)
-        print(x2, y2, z2)
+	for k,v in pairs(GetStreamedPlayersForPlayer(player)) do
+        local x2, y2, z2 = GetPlayerLocation(v)
 		local dist = GetDistance3D(x, y, z, x2, y2, z2)
 
-		if dist < 5.0 then
+		if dist < 50 then
 			return k
 		end
 	end
@@ -49,9 +43,7 @@ function GetNearestPlayer(player)
 end
 
 AddCommand("rape", function(player, instigator)
-        
       CallEvent("rape", player, instigator)
-
 end)
 
 AddCommand("rf", function(player)
