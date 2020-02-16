d81585be22f537ccae0cf84a4277452fd2a9945a
REDRUM <zerkyoags@gmail.com>
2020-02-16 12:51:06 -0800

    modified:   achievements/server.lua

--- achievements/server.lua
+++ achievements/server.lua
@@ -84,27 +84,3 @@ function OnPlayerSteamAuth(player)
     CreateAchievementData(player)
 end
 AddEvent("OnPlayerSteamAuth", OnPlayerSteamAuth)
-
---[[
-local achievements = {
-    oneday = {
-        name = "One work day",
-        desc = "8 hours of play time.",
-    },
-    workday = {
-        name = "One full day",
-        desc = "24 hours of play time."
-    },
-    millionaire = {
-        name = "Millionaire",
-        desc = "Obtain 1 million dollars."
-    },
-    bitcoin = {
-        name = "Crypto is the way to go.",
-        desc = "Buy the bitcoin company upgrade."
-    },
-    secret = {
-        name = "Its a secret...",
-        desc = "has said the secret phrase."
-    }
-}]]
